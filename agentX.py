f6f3c760221c890b5098c12c70ec0feca1ada5b2
Vilhjálmur Kári <vilhjalmurkj@gmail.com>
2018-11-26 14:04:23 +0000

    agentx

--- agentX.py
+++ agentX.py
@@ -16,26 +16,18 @@ perceive the board as player 1
 import numpy as np
 import torch
 from torch.autograd import Variable
-#import Backgammon
-import BG_Competition
-#import flipped_agent
+import Backgammon
+import flipped_agent
 device = torch.device('cpu')
 
 
 # load to global memory
-w1 = torch.load('C_w1_trained_99000.pth')
-w2 = torch.load('C_w2_trained_99000.pth')
-b1 = torch.load('C_b1_trained_99000.pth')
-b2 = torch.load('C_b2_trained_99000.pth')
+w1 = torch.load('X_w1_trained_99.pth')
+w2 = torch.load('X_w2_trained_99.pth')
+b1 = torch.load('X_b1_trained_99.pth')
+b2 = torch.load('X_b2_trained_99.pth')
 nx = 24 * 2 * 6 + 4
 
-Xw1 = torch.load('X_w1_trained_99.pth')
-Xw2 = torch.load('X_w2_trained_99.pth')
-Xb1 = torch.load('X_b1_trained_99.pth')
-Xb2 = torch.load('X_b2_trained_99.pth')
-Xnx = 24 * 2 * 6 + 4
-
-'''
 def one_hot_encoding(board, nSecondRoll):
     oneHot = np.zeros(24 * 2 * 6 + 4 + 1)
     # where are the zero, single, double, ... discs
@@ -55,76 +47,21 @@ def one_hot_encoding(board, nSecondRoll):
     oneHot[12 * 24 + 3] = board[28]
     oneHot[12 * 24 + 4] = nSecondRoll
     return oneHot
-'''
 
-def one_hot_encoding(board):
-    one_hot = []
-    for i in range(1,len(board)):
-        #create a vector with all possible quantities
-        one_hot_place = np.zeros( (2 * 15) + 1 )
-        
-        if(board[i] == 0):    
-            place_in_vector = 0
-        elif (board[i] > 0):
-            place_in_vector = int(board[i])
-        else:
-            place_in_vector = 15 + -1*int(board[i])
-        
-        one_hot_place[place_in_vector] = 1
-        one_hot.extend(one_hot_place)
-    return one_hot
-
-
-def chooseMove(board, possible_moves, possible_boards, player):
-    va = np.zeros(len(possible_moves))
-    xa = np.zeros((len(possible_moves),868))
-    for i in range(0,len(possible_moves)):
-        xa[i,:] = one_hot_encoding(possible_boards[i])
-    x = Variable(torch.tensor(xa.transpose(), dtype = torch.float, device = device))
-    h = torch.mm(w1,x) + b1
-    h_sigmoid = h.sigmoid()
-    #pi = torch.mm(theta,h_sigmoid).softmax(1)
-    #xtheta_mean = torch.sum(torch.mm(h_sigmoid,torch.diagflat(pi)),1)
-    #xtheta_mean = torch.unsqueeze(xtheta_mean,1)
-    #m = torch.multinomial(pi,1)
-    y = torch.mm(w2,h_sigmoid)+ b2
-    va = y.sigmoid().detach().cpu()
-    bestMove = np.argmax(va)
-    return possible_boards[bestMove],possible_moves[bestMove]
-
-def flip_board(board_copy):
-    #flips the game board and returns a new copy
-    idx = np.array([0,24,23,22,21,20,19,18,17,16,15,14,13,
-    12,11,10,9,8,7,6,5,4,3,2,1,26,25,28,27])
-    flipped_board = -np.copy(board_copy[idx])
-        
-    return flipped_board
-
-def flip_move(move):
-    if len(move)!=0:
-        for m in move:
-            for m_i in range(2):
-                m[m_i] = np.array([0,24,23,22,21,20,19,18,17,16,15,14,13,
-                                12,11,10,9,8,7,6,5,4,3,2,1,26,25,28,27])[m[m_i]]        
-    return move
 
 # this epsilon greedy policy uses a feed-forward neural network to approximate the after-state value function
 def action(board, dice, oplayer, i = 0):
 
     flippedplayer = -1
     if (flippedplayer == oplayer): # view it from player 1 perspective
-        board = flip_board(np.copy(board))
+        board = flipped_agent.flip_board(np.copy(board))
         player = -oplayer # player now the other player +1
-        playerTemp = oplayer
     else:
         player = oplayer
-        playerTemp = oplayer
-    possible_moves, possible_boards = BG_Competition.legal_moves(board, dice, player)
+    possible_moves, possible_boards = Backgammon.legal_moves(board, dice, player)
     na = len(possible_boards)
     if (na == 0):
         return []
-    
-    '''
     xa = np.zeros((na,nx+1))
     va = np.zeros((na))
     for j in range(0, na):
@@ -138,12 +75,4 @@ def action(board, dice, oplayer, i = 0):
     action = possible_moves[np.argmax(va)]
     if (flippedplayer == oplayer): # map this move to right view
         action = flipped_agent.flip_move(action)
-    '''
-    after_state,action = chooseMove(board, possible_moves, possible_boards, player)
-    
-    move = action
-    if playerTemp == -1: move = flip_move(action)
-    if playerTemp:
-        return move
-    
     return action
